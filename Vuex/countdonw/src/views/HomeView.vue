8ae709ff5b1b56c97c28fc315861d361fac4a0f8
Christian Silva <tininjacms@gmail.com>
2025-01-09 18:54:51 -0300

    pratica vuex

---
+++ Vuex/countdonw/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <div>Contador</div>
+    <div>{{$store.state.count}}</div>
+    <div>
+      <button class="bg-green-700">+</button>
+      <button class="bg-red-800">-</button>
+    </div>
+  </div>
+</template>
+<script src="./HomeView.ts"></script>
+<style scoped src="./HomeView.css"></style>
